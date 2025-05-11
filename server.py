21071330455cfccd227276bd407c2b167bc99d3f
matintalkhbe00 <kmmatin66@gmail.com>
2025-05-11 23:06:38 +0330

    use render.com

---
+++ server.py
@@ -0,0 +1,11 @@
+# server.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is running!"  # این همون چیزی هست که UptimeRobot می‌بینه
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
